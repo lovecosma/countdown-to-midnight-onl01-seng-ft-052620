0c46565edd47894f37eabb33c1c908afa59015ad
candescent-torrent-9701 <williamsmatthew333@gmail.com>
2020-05-26 21:41:02 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
 #write your code here
 
-def countdown
+def countdown(seconds)
+@seconds = seconds
+until @seconds <= 0
+puts "#{@seconds} SECOND(S)!"
+@seconds -= 1
+end
+return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep (seconds)
+sleep(5)
 end
