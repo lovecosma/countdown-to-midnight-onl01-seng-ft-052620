5b038714f1fbc1565a074afacac1b8b51c5815ec
candescent-torrent-9701 <williamsmatthew333@gmail.com>
2020-05-26 18:52:09 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,7 +4,7 @@ def countdown(seconds)
   @seconds = seconds
 until @seconds == -1
   puts "#{@seconds} SECOND(s)!"
-  @seconds -= 1
+  @seconds.abs -= 1
 end
 if  @seconds == 0
   return "HAPPY NEW YEAR!"
